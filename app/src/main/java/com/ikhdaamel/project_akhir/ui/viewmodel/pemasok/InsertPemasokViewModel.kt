28e768e63e125742d154a6055357e7ab8e453837
ikhdaaakmalia <punyaamelaja@gmail.com>
2025-01-20 17:05:34 +0700

    create fun InsertPemasokEvent & UiStatePemasok

--- app/src/main/java/com/ikhdaamel/project_akhir/ui/viewmodel/pemasok/InsertPemasokViewModel.kt
+++ app/src/main/java/com/ikhdaamel/project_akhir/ui/viewmodel/pemasok/InsertPemasokViewModel.kt
@@ -2,7 +2,18 @@ package com.ikhdaamel.project_akhir.ui.viewmodel.pemasok
 
 import com.ikhdaamel.project_akhir.model.Pemasok
 
-fun Pemasok.toInsertProdukUiEvent(): InsertProdukUiEvent = InsertProdukUiEvent(
+fun InsertPemasokUiEvent.toPemasok(): Pemasok = Pemasok(
+    idPemasok = idPemasok,
+    namaPemasok = namaPemasok,
+    alamatPemasok = alamatPemasok,
+    telpPemasok = telpPemasok
+)
+
+fun Pemasok.toUiStatePemasok(): InsertPemasokUiState = InsertPemasokUiState(
+    insertPemasokUiEvent = toInsertPemasokUiEvent()
+)
+
+fun Pemasok.toInsertPemasokUiEvent(): InsertPemasokUiEvent = InsertPemasokUiEvent(
     idPemasok = idPemasok,
     namaPemasok = namaPemasok,
     alamatPemasok = alamatPemasok,
