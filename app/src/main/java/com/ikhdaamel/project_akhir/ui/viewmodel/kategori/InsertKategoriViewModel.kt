acdc5c497e4150e455cc843ffbeee699f62f8615
ikhdaaakmalia <punyaamelaja@gmail.com>
2025-01-20 17:56:34 +0700

    create class InsertKategoriViewModel

--- app/src/main/java/com/ikhdaamel/project_akhir/ui/viewmodel/kategori/InsertKategoriViewModel.kt
+++ app/src/main/java/com/ikhdaamel/project_akhir/ui/viewmodel/kategori/InsertKategoriViewModel.kt
@@ -1,6 +1,31 @@
 package com.ikhdaamel.project_akhir.ui.viewmodel.kategori
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.ikhdaamel.project_akhir.model.Kategori
+import com.ikhdaamel.project_akhir.repository.repository.KategoriRepository
+import kotlinx.coroutines.launch
+
+class InsertKategoriViewModel (private val kategoriRepository: KategoriRepository) : ViewModel(){
+    var kategorikUiState by mutableStateOf(InsertKategoriUiState())
+        private set
+    fun updateInsertKategoriState(insertKategoriUiEvent: InsertKategoriUiEvent){
+        kategorikUiState = InsertKategoriUiState(insertKategoriUiEvent = insertKategoriUiEvent)
+    }
+    suspend fun insertKategori(){
+        viewModelScope.launch{
+            try {
+                kategoriRepository.insertKategori(kategorikUiState.insertKategoriUiEvent.toKategori())
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
 
 data class InsertKategoriUiState(
     val insertKategoriUiEvent: InsertKategoriUiEvent = InsertKategoriUiEvent()
