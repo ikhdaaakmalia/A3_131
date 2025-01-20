6eafa1220d2447730f8564033bf6f27bf837b296
ikhdaaakmalia <punyaamelaja@gmail.com>
2025-01-20 20:33:36 +0700

    create class HomeMerkViewModel

--- app/src/main/java/com/ikhdaamel/project_akhir/ui/viewmodel/merk/HomeMerkViewModel.kt
+++ app/src/main/java/com/ikhdaamel/project_akhir/ui/viewmodel/merk/HomeMerkViewModel.kt
@@ -1,10 +1,48 @@
 package com.ikhdaamel.project_akhir.ui.viewmodel.merk
 
+import retrofit2.HttpException
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.ikhdaamel.project_akhir.model.Merk
-import com.ikhdaamel.project_akhir.model.Pemasok
+import com.ikhdaamel.project_akhir.repository.repository.MerkRepository
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 sealed class HomeMerkUiState{
     data class Success(val merk: List<Merk>) : HomeMerkUiState()
     object Error: HomeMerkUiState()
     object Loading: HomeMerkUiState()
+}
+class HomeMerkViewModel(private val merk: MerkRepository): ViewModel(){
+    var merkUIState: HomeMerkUiState by mutableStateOf(HomeMerkUiState.Loading)
+        private set
+    init {
+        getMerk()
+    }
+    fun getMerk(){
+        viewModelScope.launch {
+            merkUIState = HomeMerkUiState.Loading
+            merkUIState = try {
+                HomeMerkUiState.Success(merk.getMerk())
+            } catch (e: IOException) {
+                HomeMerkUiState.Error
+            } catch (e: HttpException) {
+                HomeMerkUiState.Error
+            }
+        }
+    }
+    fun deleteMerk(idMerk: String){
+        viewModelScope.launch {
+            try {
+                merk.deleteMerk(idMerk)
+            } catch (e: IOException) {
+                HomeMerkUiState.Error
+            } catch (e: HttpException) {
+                HomeMerkUiState.Error
+            }
+        }
+    }
 }
