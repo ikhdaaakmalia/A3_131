38a7175f2215fc247d72bc7590d9538cc1f7c15d
ikhdaaakmalia <punyaamelaja@gmail.com>
2025-01-20 17:40:48 +0700

    create class InsertPemasokViewModel

--- app/src/main/java/com/ikhdaamel/project_akhir/ui/viewmodel/pemasok/InsertPemasokViewModel.kt
+++ app/src/main/java/com/ikhdaamel/project_akhir/ui/viewmodel/pemasok/InsertPemasokViewModel.kt
@@ -1,6 +1,30 @@
 package com.ikhdaamel.project_akhir.ui.viewmodel.pemasok
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.ikhdaamel.project_akhir.model.Pemasok
+import com.ikhdaamel.project_akhir.repository.repository.PemasokRepository
+import kotlinx.coroutines.launch
+
+class InsertPemasokViewModel (private val pemasokRepository: PemasokRepository) : ViewModel(){
+    var pemasokUiState by mutableStateOf(InsertPemasokUiState())
+        private set
+    fun updateInsertPemasokState(insertPemasokUiEvent: InsertPemasokUiEvent){
+        pemasokUiState = InsertPemasokUiState(insertPemasokUiEvent = insertPemasokUiEvent)
+    }
+    suspend fun insertPemasok(){
+        viewModelScope.launch{
+            try {
+                pemasokRepository.insertPemasok(pemasokUiState.insertPemasokUiEvent.toPemasok())
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
 
 data class InsertPemasokUiState(
     val insertPemasokUiEvent : InsertPemasokUiEvent = InsertPemasokUiEvent()
