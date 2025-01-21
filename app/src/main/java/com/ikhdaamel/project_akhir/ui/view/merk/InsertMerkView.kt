e5babddf410dbfd104e2e24bdf3487f54d2019a4
ikhdaaakmalia <punyaamelaja@gmail.com>
2025-01-21 18:45:54 +0700

    create fungsi InputMerkBody & InputMerkView

--- app/src/main/java/com/ikhdaamel/project_akhir/ui/view/merk/InsertMerkView.kt
+++ app/src/main/java/com/ikhdaamel/project_akhir/ui/view/merk/InsertMerkView.kt
@@ -4,27 +4,111 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.ikhdaamel.project_akhir.ui.customewidget.CustomeTopAppBar
+import com.ikhdaamel.project_akhir.ui.navigation.DestinasiNavigasi
+import com.ikhdaamel.project_akhir.ui.viewmodel.PenyediaViewModel
 import com.ikhdaamel.project_akhir.ui.viewmodel.merk.InsertMerkUiEvent
-import com.ikhdaamel.project_akhir.ui.viewmodel.pemasok.InsertPemasokUiEvent
+import com.ikhdaamel.project_akhir.ui.viewmodel.merk.InsertMerkUiState
+import com.ikhdaamel.project_akhir.ui.viewmodel.merk.InsertMerkViewModel
+import kotlinx.coroutines.launch
+
+object DestinasiInsertMerk: DestinasiNavigasi {
+    override val route = "input_merk"
+    override val titleRes = "INSERT MERK"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InputMerkView(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertMerkViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CustomeTopAppBar(
+                title = DestinasiInsertMerk.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ){ innerpadding ->
+        InputMerkBody(
+            insertMerkUiState = viewModel.merkUiState,
+            onMerkValueChange = viewModel::updateInsertMerkState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertMerk()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerpadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+
+@Composable
+fun InputMerkBody(
+    insertMerkUiState: InsertMerkUiState,
+    onMerkValueChange:(InsertMerkUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInputMerk(
+            insertMerkUiEvent = insertMerkUiState.insertMerkUiEvent,
+            onValueChange = onMerkValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "SIMPAN")
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
